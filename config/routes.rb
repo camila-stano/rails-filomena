ba0289c065e21df2edb4f9cc0252f19167dc2a53
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-02-22 20:03:26 -0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :clothes do
+    resources :trades, except: :destroy do
+      get 'reviews', to: 'trades#reviews_new', as: :reviews_new
+      post 'reviews', to: 'trades#reviews_create'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
