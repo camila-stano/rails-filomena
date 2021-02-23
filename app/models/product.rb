d5f48cdbb79868b8ac312964aa2e906ff807a008
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-02-23 19:55:57 -0300

    add view products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :trades
   has_many_attached :photos
+
+  CATEGORIES = %w[Casual Festa Rolê]
+  PRODUCT_TYPE = %w[Shorts Camiseta Vestido]
+  SIZE = %w[PP P M G GG XGG 38 40 42 44 46 48 50 52 54 56 58 60]
 end
