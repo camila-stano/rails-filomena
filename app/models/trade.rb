c23af6672f3260058d6bbfd607e483cd1f61b822
Mariana Marinho <marianaapmarinho@gmail.com>
2021-02-25 18:10:25 -0300

    páginas de show com banner ok, index atualizado

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -2,5 +2,12 @@ class Trade < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
+  after_update :update_rating
+
   validates :content, presence: true, length: { minimum: 5 }
+
+  def update_rating
+    new_rating = product.user.trades.reduce(0.0) { |total, trade| trade.review_rating + total } / product.user.trades.count
+    product.user.update_attribute(:rating, new_rating.round(1))
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_184155) do
+ActiveRecord::Schema.define(version: 2021_02_25_201644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2021_02_24_184155) do
     t.date "bday"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "rating", precision: 2, scale: 1
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
