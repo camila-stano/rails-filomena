f901840b1d3c431d77011d233591c7bfcea01d14
Mariana Marinho <marianaapmarinho@gmail.com>
2021-02-26 12:59:48 -0300

    alteração no footer, falta mexer na cor

--- db/migrate/20210226152945_create_rejects.rb
+++ db/migrate/20210226152945_create_rejects.rb
@@ -1,7 +1,7 @@
 class CreateRejects < ActiveRecord::Migration[6.0]
   def change
     create_table :rejects do |t|
-      t.boolean :reject, default: true 
+      t.boolean :reject, default: false 
       t.references :users, null: false, foreign_key: true
       t.references :products, null: false, foreign_key: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_201644) do
+ActiveRecord::Schema.define(version: 2021_02_26_152945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,16 @@ ActiveRecord::Schema.define(version: 2021_02_25_201644) do
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
+  create_table "rejects", force: :cascade do |t|
+    t.boolean "reject", default: false
+    t.bigint "users_id", null: false
+    t.bigint "products_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["products_id"], name: "index_rejects_on_products_id"
+    t.index ["users_id"], name: "index_rejects_on_users_id"
+  end
+
   create_table "trades", force: :cascade do |t|
     t.boolean "super_like"
     t.string "status", default: "incomplete"
@@ -89,6 +99,8 @@ ActiveRecord::Schema.define(version: 2021_02_25_201644) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "products", "users"
+  add_foreign_key "rejects", "products", column: "products_id"
+  add_foreign_key "rejects", "users", column: "users_id"
   add_foreign_key "trades", "products"
   add_foreign_key "trades", "users"
 end
