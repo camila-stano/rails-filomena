c1923493cd2c789d613285739bda2ccfbc994358
Mariana Marinho <marianaapmarinho@gmail.com>
2021-02-26 12:39:03 -0300

    validações de foto de produto

--- db/migrate/20210226152945_create_rejects.rb
+++ db/migrate/20210226152945_create_rejects.rb
@@ -1,6 +1,7 @@
 class CreateRejects < ActiveRecord::Migration[6.0]
   def change
     create_table :rejects do |t|
+      t.boolean :reject, default: true 
       t.references :users, null: false, foreign_key: true
       t.references :products, null: false, foreign_key: true
 
